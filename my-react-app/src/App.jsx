897f8aa59e5bb81154bb0d558e6e22ce90507db1
Fran Maznik <fm53363@fer.hr>
2024-05-07 18:10:39 +0200

    move components to separate folders

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,37 +1,42 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios'; // Import Axios
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios"; // Import Axios
+import "./App.css";
 
-import Navbar from './Navbar';
-import Sales from './Sales';
-import Home from './Home';
+import Navbar from "./components/Navbar/Navbar";
+import Sales from "./components/Sales/Sales";
+import Home from "./components/Home/Home";
 
-
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
- 
-
   const navLinks = [
-    { label: 'Croatia', path: '/croatia', server: 'hr' },
-    { label: 'Slovenia', path: '/slovenia', server: 'slo' }
+    { label: "Croatia", path: "/croatia", server: "hr" },
+    { label: "Slovenia", path: "/slovenia", server: "slo" },
   ];
 
-
   return (
-    <Router>
-      <div>
-        <Navbar links={navLinks} />
-        <hr></hr>
+    <div className="App">
+      <Router>
+        <div>
+          <Navbar links={navLinks} />
+          <hr></hr>
 
-        <Routes>
-         <Route path="/" element={<Home />} /> {}
-
-          <Route exact path="/croatia" element={<Sales country="Croatia" server="hr"/>} />
-          <Route exact path="/slovenia" element={ <Sales country="Slovenia" server="slo"/>} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/" element={<Home />} /> {}
+            <Route
+              exact
+              path="/croatia"
+              element={<Sales country="Croatia" server="hr" />}
+            />
+            <Route
+              exact
+              path="/slovenia"
+              element={<Sales country="Slovenia" server="slo" />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
-};
+}
 export default App;
